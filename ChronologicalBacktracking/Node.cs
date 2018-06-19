9fb1cd111ffeec0f8d49148a0346e4d16edf208b
John <HylianNanobyte@Gmail.com>
2018-06-19 15:00:53 +0200

    start

--- ChronologicalBacktracking/Node.cs
+++ ChronologicalBacktracking/Node.cs
@@ -6,13 +6,34 @@ using System.Threading.Tasks;
 
 namespace ChronologicalBacktracking {
     struct Node {
-        int x, y;
-        List<int> possibleValues;
+        public int x, y;
+        public List<int> possibleValues;
+        public bool changable;
 
-        Node(int x, int y, List<int> possibleValues) {
+        public Node(int x, int y, List<int> possibleValues) {
             this.x = x;
             this.y = y;
             this.possibleValues = possibleValues;
+            changable = true;
+        }
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+            possibleValues = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            changable = true;
+        }
+
+        public int RemovePossibility(int value) {
+            if (possibleValues.Contains(value)) {
+                possibleValues.Remove(value);
+            }
+            return possibleValues.Count;
+        }
+
+        public void SetUnchangable(int value) {
+            possibleValues = new List<int>() { value };
+            changable = false;
         }
     }
 }
