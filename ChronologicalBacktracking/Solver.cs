f2238ad1decfcc783c15bccc1ab1620ebcca33f0
Bernd van den Hoek <bernd_hoek@hotmail.com>
2018-06-19 13:52:11 +0200

    Add classes

---
+++ ChronologicalBacktracking/Solver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronologicalBacktracking
+{
+    class Solver
+    {
+        Backtracker backtracker;
+        Sudoko sudoko;
+
+        public Solver(Backtracker backtracker, Sudoko sudoko)
+        {
+            this.backtracker = backtracker;
+            this.sudoko = sudoko;
+        }
+    }
+}
