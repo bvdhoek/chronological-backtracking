ead365a872150ac66b0912c9a5ca054d45fc6e66
John <HylianNanobyte@Gmail.com>
2018-06-19 14:21:23 +0200

    added node and some gitignores

---
+++ ChronologicalBacktracking/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronologicalBacktracking {
+    struct Node {
+        int x, y;
+        List<int> possibleValues;
+
+        Node(int x, int y, List<int> possibleValues) {
+            this.x = x;
+            this.y = y;
+            this.possibleValues = possibleValues;
+        }
+    }
+}
