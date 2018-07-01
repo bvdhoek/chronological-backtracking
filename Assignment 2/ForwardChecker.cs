b0ccaaa8b88711451c57d9559b274dc4db65843f
John <HylianNanobyte@Gmail.com>
2018-07-01 15:22:28 +0200

    start of forward checker

--- Assignment 2/ForwardChecker.cs
+++ Assignment 2/ForwardChecker.cs
@@ -5,6 +5,92 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Assignment_2 {
-    class ForwardChecker {
+    class ForwardChecker : Solver {
+        public ForwardChecker(int[,] values) : base(values) {
+            if (Solve(cells[0, 0])) {
+                PrintCells();
+            } else {
+                Console.WriteLine("Sudoku could not be solved");
+            }
+        }
+
+        public override bool Solve(Cell cell) {
+            if (cell == null)
+                return true;
+            if (cell.value != 0) {
+                CheckForward(cell, cell.value);
+                return Solve(NextCell(cell));
+            }
+
+            for (int value = 1; value <= 9; value++) {
+                if (!cell.domain.Contains(value))
+                    continue;
+                
+                cell.value = value;
+                CheckForward(cell, value);
+                if (Solve(NextCell(cell))) {
+                    return true;
+                } else {
+                    cell.value = 0;
+                }
+            }
+
+            return false;
+
+        }
+
+        public override Cell NextCell(Cell cell) {
+            int x = cell.x;
+            int y = cell.y;
+
+            if (cell.x == 8) {
+                x = 0;
+                y++;
+            } else {
+                x++;
+            }
+
+            if (y > 8)
+                return null;
+            return cells[x, y];
+        }
+
+        public override bool SatisfiesConstraints(Cell cell, int value) {
+            for (int i = 0; i < 9; i++) {
+                if (cells[i, cell.y].value == value || cells[cell.x, i].value == value)
+                    return false;
+            }
+
+            int x1 = 3 * (cell.x / 3);
+            int y1 = 3 * (cell.y / 3);
+            int x2 = x1 + 2;
+            int y2 = y1 + 2;
+
+            for (int x = x1; x <= x2; x++) {
+                for (int y = y1; y <= y2; y++) {
+                    if (cells[x, y].value == value)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public void CheckForward(Cell cell, int value) {
+            for (int i = 0; i < 9; i++) {
+                cells[i, cell.y].domain.Remove(value);
+                cells[cell.x, i].domain.Remove(value);
+            }
+
+            int x1 = 3 * (cell.x / 3);
+            int y1 = 3 * (cell.y / 3);
+            int x2 = x1 + 2;
+            int y2 = y1 + 2;
+
+            for (int x = x1; x <= x2; x++) {
+                for (int y = y1; y <= y2; y++) {
+                    cells[x, y].domain.Remove(value);
+                }
+            }
+        }
     }
 }
